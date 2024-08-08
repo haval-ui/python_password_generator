eba82187912c6da3b5c692f5df9ed5da8c0e9997
haval-ui <havalkoey00908@gmail.com>
2024-08-08 15:00:19 +0300

    the final commit

--- index.py
+++ index.py
@@ -1,12 +1,30 @@
-student_hights=input("enter the students hights: ").split()
+#Password Generator Project
+import random
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
-for n in range(0,len(student_hights)):
-    student_hights[n]=int(student_hights[n])
+print("Welcome to the PyPassword Generator!")
+nr_letters= int(input("How many letters would you like in your password?\n")) 
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
 
-number_of_students=0
-hight_sum=0
-for i in student_hights:
-     hight_sum+=i
-     number_of_students+=1
-avg=hight_sum/number_of_students
-print(f"the average hight is: {avg} ")  
+
+password=""
+#Eazy Level - Order not randomised:
+#e.g. 4 letter, 2 symbol, 2 number = JduE&!91
+
+for n in range(0,nr_letters):
+    password=password+random.choice(letters)
+
+for n in range(0,nr_numbers):    
+    password=password+random.choice(numbers)
+
+for n in range(0,nr_symbols):
+    password=password+random.choice(symbols)
+print(password)
+
+#Hard Level - Order of characters randomised:
+#e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
+randomized_password=''.join(random.sample(password,len(password)))
+print(randomized_password)
